1f3abffecbfea85cac5063e9a83ea2934fba750b
Juraj Mudroch <mudroch.juraj@protonmail.com>
2025-01-17 12:05:07 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Mulish: [400, 600, 900] // Vyber hrúbky písma, ktoré potrebuješ
+    },
+    display: 'swap', // Odporúčané pre lepší výkon
+  },
   css: ['@/assets/scss/global.scss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
